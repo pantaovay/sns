6343cc9a56a7ed9af4b8d9bbd051671a6341664e
Dongxi Huang <huangdongxi@yoloyolo.tv>
2017-05-20 15:35:44 +0800

    修改命名

--- src/Open/Alipay.php
+++ src/Open/Alipay.php
@@ -5,25 +5,25 @@ class Alipay
 {
     const ALIPAY_URI = 'https://openapi.alipay.com/gateway.do';
 
-    public static function getUserInfoParams($appId, $accessToken, $rsaPrivateKey, $singType)
+    public static function getUserInfoParams($appId, $accessToken, $rsaPrivateKey, $signType)
     {
         $paramsArray = [
             'app_id' => $appId,
             'format' => 'json',
             'method' => 'alipay.user.userinfo.share',
             'charset' => 'UTF-8',
-            'sign_type' => $singType,
+            'sign_type' => $signType,
             'timestamp' => date('Y-m-d H:i:s'),
             'version' => '1.0',
             'auth_token' => $accessToken,
         ];
 
-        $paramsArray['sign'] = self::getSign($paramsArray, $rsaPrivateKey, $singType);
+        $paramsArray['sign'] = self::getSign($paramsArray, $rsaPrivateKey, $signType);
 
         return $paramsArray;
     }
 
-    private static function getSign(array $data, $rsaPrivateKey, $singType) : string
+    private static function getSign(array $data, $rsaPrivateKey, $signType) : string
     {
         ksort($data);
 
@@ -34,7 +34,7 @@ class Alipay
 
         $dataString = substr($dataString, 0, -1);
 
-        if ($singType == 'RSA2') {
+        if ($signType == 'RSA2') {
             openssl_sign($dataString, $sign, $rsaPrivateKey, OPENSSL_ALGO_SHA256);
         } else {
             openssl_sign($dataString, $sign, $rsaPrivateKey);
